1ca2887338c13a191fab145e06054fee16369728
Maryna Binko <benyhorses@hotmail.com>
2024-05-12 10:27:44 +0100

    card flex

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
-import styles from "./FriendListItem.module.css"
+import styles from "./FriendListItem.module.css";
+import clsx from "clsx";
 
 const FriendListItem = ({avatar, name, isOnline }) => {
+    const className = clsx(
+        styles.friendCard, 
+        styles.friendsList
+         );
+
   return (
-    <div className={styles.friendCard}>
+    <div className={className}>
       <img className={styles.friendImg} src={avatar} alt="Avatar" width="48" />
       <p className={styles.friendName}>{name}</p>
       <div>{isOnline ? <p className={styles.online}>Online</p> 
